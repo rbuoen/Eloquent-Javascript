e4b898819b742a4df7ff0636767d891b245f73ac
Riley Buoen <58043852+rbuoen@users.noreply.github.com>
2021-06-13 15:03:46 -0700

    Update exercises.js

--- Chapter-4-Data-Structures-Exercises/exercises.js
+++ Chapter-4-Data-Structures-Exercises/exercises.js
@@ -34,15 +34,13 @@ function reverseArray(array) {
 }
 
 function reverseArrayInPlace(array) {
-    for (let i = array.length; i >= 0; i--) {
-        console.log(array[i]);
-        array[i] = array[i - array.length];
-    }
+    console.log(array.length/2);
 }
+
 
 console.log(reverseArray(["A", "B", "C"]));
 // → ["C", "B", "A"];
-let arrayValue = [1, 2, 3, 4, 5];
+var arrayValue = [1, 2, 3, 4, 5];
 reverseArrayInPlace(arrayValue);
 console.log(arrayValue);
 // → [5, 4, 3, 2, 1]
