a7ca114fa18f49c69569d3ab2001d3711ce5acdb
Riley <buoenriley@gmail.com>
2021-06-14 11:06:05 -0700

    Working on question 3
    completed arrayToList fucntion

--- Chapter-4-Data-Structures-Exercises/exercises.js
+++ Chapter-4-Data-Structures-Exercises/exercises.js
@@ -1,58 +1,77 @@
-// June 12, 2021
+// // June 12, 2021
 
-// The sum of a Range
+// // The sum of a Range
 
-// returns and array containing a range of numbers
-function range(start, end, step = start < end ? 1 : -1) {
-    array = [];
-    for (let i = start; start < end ? i <= end : i >= end; i += step) array.push(i);
-    return array;
-}
+// // returns and array containing a range of numbers
+// function range(start, end, step = start < end ? 1 : -1) {
+//     array = [];
+//     for (let i = start; start < end ? i <= end : i >= end; i += step) array.push(i);
+//     return array;
+// }
 
-// returns the sum of an array of numbers
-function sum(array) {
-    let sum = 0;
-    for (num of array) sum += num;
-    return sum;
-}
+// // returns the sum of an array of numbers
+// function sum(array) {
+//     let sum = 0;
+//     for (num of array) sum += num;
+//     return sum;
+// }
 
-console.log(range(1, 10));
-// → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-console.log(range(5, 2, -1));
-// → [5, 4, 3, 2]
-console.log(sum(range(1, 10)));
-// → 55
+// console.log(range(1, 10));
+// // → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// console.log(range(5, 2, -1));
+// // → [5, 4, 3, 2]
+// console.log(sum(range(1, 10)));
+// // → 55
 
-// Reversing an Array
+// // Reversing an Array
+
+// // function reverseArray(array) {
+// //     let outArray = [];
+// //     for (item of array) { 
+// //         outArray.unshift(item) 
+// //     }
+// //     return outArray;
+// // }
 
 // function reverseArray(array) {
-//     let outArray = [];
-//     for (item of array) { 
-//         outArray.unshift(item) 
+//     const outArray = [];
+//     for (let i = array.length - 1; i >= 0; i--) {
+//         outArray.push(array[i]);
 //     }
 //     return outArray;
 // }
 
-function reverseArray(array) {
-    const outArray = [];
+// function reverseArrayInPlace(array) {
+//     for (let i = 0; i < Math.floor(array.length / 2); i++) {
+//         let old = array[i];
+//         array[i] = array[array.length - 1 - i];
+//         array[array.length - 1 - i] = old;
+//     }
+//     return array;
+// }
+
+// console.log(reverseArray(["A", "B", "C"]));
+// // → ["C", "B", "A"];
+// var arrayValue = [1, 2, 3, 4, 5];
+// reverseArrayInPlace(arrayValue);
+// console.log(arrayValue);
+// // → [5, 4, 3, 2, 1]
+
+// A List
+
+function arrayToList(array) {
+    let list = null;
     for (let i = array.length - 1; i >= 0; i--) {
-        outArray.push(array[i]);
+        list = {value: array[i], rest: list}
     }
-    return outArray;
+   return list;
 }
 
-function reverseArrayInPlace(array) {
-    for (let i = 0; i < Math.floor(array.length / 2); i++) {
-        let old = array[i];
-        array[i] = array[array.length - 1 - i];
-        array[array.length - 1 - i] = old;
-    }
-    return array;
-}
-
-console.log(reverseArray(["A", "B", "C"]));
-// → ["C", "B", "A"];
-var arrayValue = [1, 2, 3, 4, 5];
-reverseArrayInPlace(arrayValue);
-console.log(arrayValue);
-// → [5, 4, 3, 2, 1]
+console.log(arrayToList([10, 20]));
+// → {value: 10, rest: {value: 20, rest: null}}
+// console.log(listToArray(arrayToList([10, 20, 30])));
+// → [10, 20, 30]
+// console.log(prepend(10, prepend(20, null)));
+// → {value: 10, rest: {value: 20, rest: null}}
+// console.log(nth(arrayToList([10, 20, 30]), 1));
+// → 20
