09166e280971e549759958889251e84b999071c3
Riley <buoenriley@gmail.com>
2021-06-17 12:15:13 -0700

    completed exercise

--- Chapter-4-Data-Structures-Exercises/a-list.js
+++ Chapter-4-Data-Structures-Exercises/a-list.js
@@ -16,11 +16,25 @@ function listToArray(list) {
     return array;
 }
 
+function prepend(element, list) {
+    return {value: element, rest: list};
+  }
+
+function nth(list, pos) {
+count = 0;
+    for (let node = list; node; node = node.rest) {
+        if (count === pos) {
+        return node.value;
+        }
+        count += 1;
+    }
+}
+
 console.log(arrayToList([10, 20]));
 // → {value: 10, rest: {value: 20, rest: null}}
 console.log(listToArray(arrayToList([10, 20, 30])));
 // → [10, 20, 30]
-// console.log(prepend(10, prepend(20, null)));
+console.log(prepend(10, prepend(20, null)));
 // → {value: 10, rest: {value: 20, rest: null}}
-// console.log(nth(arrayToList([10, 20, 30]), 1));
+console.log(nth(arrayToList([10, 20, 30]), 1));
 // → 20
